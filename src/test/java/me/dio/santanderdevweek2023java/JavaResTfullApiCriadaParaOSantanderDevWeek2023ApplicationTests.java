647fe256e1f1ac5fba43b69ec052bf95832eed1a
Ulisses Alves <ulisalves@gmail.com>
2023-10-17 21:28:10 -0300

    Java Restful API para Santander dev week 2023

---
+++ src/test/java/me/dio/santanderdevweek2023java/JavaResTfullApiCriadaParaOSantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweek2023java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaResTfullApiCriadaParaOSantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
